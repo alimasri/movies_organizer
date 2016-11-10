cb931eac68dff730954f7b5be770e8f44d047a23
Ali Masri <alimasri1991@gmail.com>
2016-11-10 16:56:25 +0100

    add a console-script in setup.cfg file remove unnecessary files

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import pytest
-from movies_organizer.skeleton import fib
-
 __author__ = "Ali Masri"
 __copyright__ = "Ali Masri"
 __license__ = "none"
-
-
-def test_fib():
-    assert fib(1) == 1
-    assert fib(2) == 1
-    assert fib(7) == 13
-    with pytest.raises(AssertionError):
-        fib(-10)
