1ceabe523b59f147a6cea2da9a8140737ab04eb8
Ali Masri <ali.masri@cognitus.fr>
2018-02-25 14:15:11 +0200

    modify the code to match the new API changes

--- movies_organizer/utils.py
+++ movies_organizer/utils.py
@@ -84,10 +84,9 @@ def list_folders(path):
             yield file
 
 
-def print_time(seconds):
-    m, s = divmod(seconds, 60)
-    h, m = divmod(m, 60)
-    return "%d:%02d:%02d" % (h, m, s)
+def print_time(mins):
+    h, m = divmod(mins, 60)
+    return "%02d:%02d" % (h, m)
 
 
 def search(movie_title, auto_select):
